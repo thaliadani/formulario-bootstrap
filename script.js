a129acef113fe1a4f6886b3b43052683f9939aac
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2026-04-13 09:34:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//Mostrar Dados do Prontuário
+//Mostrar os dados digitados do prontuário
 function mostrarDados() {
+
+  // Pegar os dados digitados no formulário
   let nome = document.getElementById("nome").value;
   let idade = document.getElementById("idade").value;
   let sexo = document.getElementById("sexo").value;
@@ -12,23 +14,26 @@ function mostrarDados() {
   let dosagem = document.getElementById("dosagem").value;
   let horario = document.getElementById("horario").value;
 
+  // Criar no html os dados digitados
   let resultado = `
-    <h5>Prontuário do Paciente<h5>
-    Nome: ${nome} <br>
-    Idade: ${idade} <br>
-    Sexo: ${sexo} <br>
-    Pressão Arterial: ${pressao} <br>
-    Temperatura: ${temperatura}°C <br>
-    Frequência Cardíaca: ${frequencia} <br>
-    Saturação: ${saturacao}% <br>
-    Evolução: ${evolucao} <br>
-    Medicamento: ${medicamento} <br>
-    Dosagem: ${dosagem} <br>
-    Horário: ${horario}
+    <ul class="list-group text-start">
+      <li class="list-group-item"><strong>Nome:</strong> ${nome}</li>
+      <li class="list-group-item"><strong>Idade:</strong> ${idade}</li>
+      <li class="list-group-item"><strong>Sexo:</strong> ${sexo}</li>
+      <li class="list-group-item"><strong>Pressão Arterial:</strong> ${pressao}</li>
+      <li class="list-group-item"><strong>Temperatura:</strong> ${temperatura}°C</li>
+      <li class="list-group-item"><strong>Frequência Cardíaca:</strong> ${frequencia}</li>
+      <li class="list-group-item"><strong>Saturação:</strong> ${saturacao}%</li>
+      <li class="list-group-item"><strong>Evolução:</strong> ${evolucao}</li>
+      <li class="list-group-item"><strong>Medicamento:</strong> ${medicamento}</li>
+      <li class="list-group-item"><strong>Dosagem:</strong> ${dosagem}</li>
+      <li class="list-group-item"><strong>Horário:</strong> ${horario}</li>
+    </ul>
 `;
 
+  // Mostrar os dados no html
   let caixa = document.getElementById("resultado");
-  caixa.innerHTML = resultado;
+  caixa.querySelector(".card-body").innerHTML = resultado;
   caixa.classList.remove("d-none");
 }
 
@@ -39,19 +44,23 @@ function mostrarDados() {
   const forms = document.querySelectorAll('.needs-validation')
   const toast = new bootstrap.Toast(document.getElementById('toastSucesso'));
 
-  // Loop over them and prevent submission
+  // Loop para validar os campos do formulário
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
+
+        // Impedir o envio do formulário
         event.preventDefault();
+
+        // Impedir a propagação padrão do evento
         event.stopPropagation();
 
       } else {
         event.preventDefault();
+
         //Mensagem de sucesso 
         toast.show();
 
-        //Mostrar os dados digitados
         mostrarDados();
       }
       form.classList.add('was-validated');
